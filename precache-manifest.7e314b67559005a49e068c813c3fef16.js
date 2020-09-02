f21476f2e1ae92e2be00617f3c0b96ff3f32ccd3
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2020-09-02 18:19:02 +0400

    Updates

---
+++ precache-manifest.7e314b67559005a49e068c813c3fef16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e139f2d86c9355e0e14ee650024c3b0",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "4e54ac2b2835d34e52c5",
+    "url": "/todo/static/css/main.1dd78cad.chunk.css"
+  },
+  {
+    "revision": "ccfc60ef3bb393590332",
+    "url": "/todo/static/js/2.eda2665f.chunk.js"
+  },
+  {
+    "revision": "ea400bfb7cfb62de59f10ac82e4a6f9d",
+    "url": "/todo/static/js/2.eda2665f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e54ac2b2835d34e52c5",
+    "url": "/todo/static/js/main.ce49ef4b.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  },
+  {
+    "revision": "69c703b21776155b4bf13adbbb72e5a5",
+    "url": "/todo/static/media/Montserrat-Bold.69c703b2.ttf"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/todo/static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "5906fb82e31864c9b531898bb3c97d98",
+    "url": "/todo/static/media/Montserrat-Regular.5906fb82.ttf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "/todo/static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "3a1997bd292d3032e2cd2bf7d230393e",
+    "url": "/todo/static/media/avatar.3a1997bd.png"
+  },
+  {
+    "revision": "261dd5d825e501e6d8bbcb8bb1c87155",
+    "url": "/todo/static/media/delete.261dd5d8.svg"
+  }
+]);
